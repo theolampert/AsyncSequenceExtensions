e8c51cd2dc5ac8d477d2bea1c579af0cf0b52b5b
Theo <mail@theo.sh>
2024-06-30 11:11:42 +0200

    throws -> rethrows

--- Sources/Sequence+concurrentMap.swift
+++ Sources/Sequence+concurrentMap.swift
@@ -2,7 +2,7 @@ extension Sequence {
     func concurrentMap<T>(
         _ transform: @escaping (Element) async throws -> T,
         priority: TaskPriority? = nil
-    ) async throws -> [T] {
+    ) async rethrows -> [T] {
         let tasks = map { element in
             Task(priority: priority) {
                 try await transform(element)
